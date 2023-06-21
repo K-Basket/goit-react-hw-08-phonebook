cd6bc9de4a03db0ca9ed87ee084b2aa4570394a2
K-Basket <koshykkv@gmail.com>
2023-06-21 13:57:59 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
-import Register from 'pages/Register/Register';
 import Login from 'pages/Login/Login';
 import Contacts from 'pages/Contacts/Contacts';
 import Home from 'pages/Home/Home';
+import Register from 'pages/Register/Register';
 
 export function App() {
   return (
